99f7645942b54dd64b14458044e1d6d8b4deee94
Clement Lecocq <hello@clementlecocq.com>
2022-04-26 17:03:26 +0200

    (SCRIPT) MAJ des commentaires du fichier cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,6 @@
+// On crée un class objet qui réunira toutes les fonctions dont nous avons besoin concernant le panier
 class Cart {
+    // On initialise l'objet à l'aide du constructor afin de récupérer le panier à chaque fois
     constructor() {
         let cart = localStorage.getItem("cart")
         if (cart == null) {
@@ -7,13 +9,22 @@ class Cart {
             this.cart = JSON.parse(cart)
         }
     }
+
+    // On crée une fonction pour enregistrer le panier dans le localstorage (avec cart en paramètre pour lui signifier quel panier enregistrer)
+    // Le localstorage ne pouvant pas enregistrer des objets complexes, on sérialise les données qu'on va "parser" par la suite
     save() {
         localStorage.setItem("cart", JSON.stringify(this.cart))
     }
 
+    // On crée une fonction pour ajouter au panier
     add(product) {
+        // On regarde ensuite si le produit est déjà dans le panier, si oui on augmente sa quantité, sinon on ajoute le produit
+        // On utilise .find qui est une fonction permettant de chercher un élément sur un tableau par rapport à une condition
+        // Pour ce site, on regarde si un produit a la même id ET la même couleur
         let foundProduct = this.cart.find(p => (p.userProductId === product.userProductId) && (p.userProductColor === product.userProductColor))
 
+        // Si il existe déjà on crée une variable de nouvelle quantité correspondante à l'addition de la quantité existante et de la valeur entrée
+        // Sinon on définit par défaut une quantité correspondante à la valeur entrée   
         if (foundProduct) {
             let newQuantity = parseInt(foundProduct.userProductQuantity) + parseInt(quantity.value)
             foundProduct.userProductQuantity = newQuantity;
@@ -24,13 +35,18 @@ class Cart {
         this.save()
     }
 
+    // On crée une fonction pour retirer un produit du panier
     remove(product) {
-        this.cart = this.cart.filter(p => (p.id != product.id) || (p.color != product.color))
+        // On utilise la fonction .filter qui va filter un tableau suivant une condition
+        // Ici il va donc conserver que les produits qui n'ont pas ni le même id, ni la même couleur
+        this.cart = this.cart.filter(p => (p.userProductId != product.userProductId) || (p.userProductColor != product.userProductColor))
         this.save()
     }
 
+    // On crée une fonction pour modifier la quantité
     changeQuantity(product, quantity) {
-        let foundProduct = this.cart.find(p => (p.id === product.id) && (p.color === product.color))
+        // On regarde si le produit est dans le panier
+        let foundProduct = this.cart.find(p => (p.userProductId === product.userProductId) && (p.userProductColor === product.userProductColor))
         if (foundProduct != undefined) {
             foundProduct.quantity += quantity
                 // On crée une condition si la valeur est en dessous de 0 car cela n'a pas de sens de vendre -x produits
