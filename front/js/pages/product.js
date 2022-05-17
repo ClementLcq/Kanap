ed9329a5395a74035e10ff386ddbd184e085dca4
Clement Lecocq <hello@clementlecocq.com>
2022-05-17 16:27:28 +0200

    (PRODUCT) MAJ error mauvaise id

--- front/js/pages/product.js
+++ front/js/pages/product.js
@@ -1,10 +1,12 @@
 const param = new URLSearchParams(window.location.search);
 let idProduct = param.get("id")
 let service = new Service()
+const positionError = document.querySelector(".item")
 service.getDetailProduct(idProduct).then(dataDetailProduct => {
     if (dataDetailProduct.error) {
         /*Error*/
-
+        const errorID = `<h4><a href="../html/index.html">Attention, le produit recherché n'existe pas, veuillez retourner sur la page d'accueil</a></h4>`
+        positionError.innerHTML = errorID
         console.log(dataDetailProduct.error)
         return false
     }
