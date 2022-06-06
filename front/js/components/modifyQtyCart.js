55157cf52ed2b6dec48913e14e5748ed4c1a0c02
Clement Lecocq <hello@clementlecocq.com>
2022-06-06 12:16:39 +0200

    (CART) MAJ modifQtyCart > ciblage data id et data color OK

--- front/js/components/modifyQtyCart.js
+++ front/js/components/modifyQtyCart.js
@@ -1,5 +1,5 @@
 const modifyQuantityCart = (idProduct, color) => {
-    const itemQuantityButton = document.querySelector(".cart__item")
+    const itemQuantityButton = document.querySelector(".cart__item[data-id='" + idProduct + "'][data-color='" + color + "']")
 
     itemQuantityButton.addEventListener("change", (e) => {
         e.preventDefault()
