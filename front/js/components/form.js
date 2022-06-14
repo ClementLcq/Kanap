700e57fa8b0a3fe6dd2638aee3c23fe3448c310b
Clement Lecocq <hello@clementlecocq.com>
2022-06-14 17:04:29 +0200

    (FORM) refacto ok - tout fonctionne

--- front/js/components/form.js
+++ front/js/components/form.js
@@ -23,61 +23,73 @@ formFields.forEach(field => {
 
 // Listen to the order click + cart verif + form verif + creation contact object and post
 const postForm = () => {
-    const orderButton = document.querySelector("#order");
-    orderButton.addEventListener("click", (e) => {
-        e.preventDefault();
+    const orderForm = document.querySelector(".cart__order__form");
+    orderForm.addEventListener("submit", (e) => {
 
         //get datas in the LS
         const cart = new Cart()
         const actualCart = cart.getCart()
 
         // verif items in cart + empty array + push method
-        if (actualCart !== null) {
-            let orderProducts = [];
-            for (let i = 0; i < actualCart.length; i++) {
-                orderProducts.push(actualCart[i].userProductId);
-            }
-
-            // Creation contact object + verif input
-            if (firstName && lastName && address && city && email) {
-                const orderUserProduct = {
-                    contact: {
-                        firstName: firstName,
-                        lastName: lastName,
-                        address: address,
-                        city: city,
-                        email: email,
-                    },
-                    products: orderProducts,
-                };
-
-                // Post method + fetch with id in URL
-                const post = {
-                    method: "POST",
-                    body: JSON.stringify(orderUserProduct),
-                    headers: {
-                        'Accept': "application/json",
-                        "Content-Type": "application/json",
-                    },
-                };
-                fetch("http://localhost:3000/api/products/order", post)
-                    .then((res) => res.json())
-                    .then((data) => {
-                        document.location.href = "confirmation.html?id=" + data.orderId;
-                        // Clear LS and fields form
-                        localStorage.clear()
-                        document.querySelector(".cart__order__form").reset();
-                    })
-                    .catch(function(err) {
-                        console.log("Erreur fetch" + err);
-                        alert("Il semblerait qu'il y ait un problème au niveau de notre API. Veuillez retenter s'il vous plait.")
-                    });
-            } else {
-                alert("Attention, il semblerait que le formulaire ne soit pas bien renseigné.");
-            }
-        } else {
+        if (actualCart.length == 0) {
+            e.preventDefault()
             alert("Attention, il semblerait que votre panier soit vide. Veuillez sélectionner des articles et recommencer.");
+            return false
         }
+
+        let orderProducts = [];
+        for (let i = 0; i < actualCart.length; i++) {
+            orderProducts.push(actualCart[i].userProductId);
+        }
+        //console.log(formFields.every(Boolean))
+        let formValid = true;
+        formFields.forEach(field => {
+            if (field.valid == false) {
+                formValid = false
+            }
+        })
+        if (formValid == false) {
+            e.preventDefault()
+            alert("Attention, il semblerait que le formulaire ne soit pas bien renseigné.");
+            return false
+        }
+        // Creation contact object + verif input
+
+        const orderUserProduct = {
+            contact: {
+                firstName: e.target.querySelector("#firstName").value,
+                lastName: e.target.querySelector("#lastName").value,
+                address: e.target.querySelector("#address").value,
+                city: e.target.querySelector("#city").value,
+                email: e.target.querySelector("#email").value,
+            },
+            products: orderProducts,
+        };
+
+        // Post method + fetch with id in URL
+        const post = {
+            method: "POST",
+            body: JSON.stringify(orderUserProduct),
+            headers: {
+                'Accept': "application/json",
+                "Content-Type": "application/json",
+            },
+        };
+        console.debug(post)
+        fetch("http://localhost:3000/api/products/order", post)
+            .then((res) => res.json())
+            .then((data) => {
+                document.location.href = "confirmation.html?id=" + data.orderId;
+                // Clear LS and fields form
+                localStorage.clear()
+                document.querySelector(".cart__order__form").reset();
+            })
+            .catch(function(err) {
+                console.log("Erreur fetch" + err);
+                alert("Il semblerait qu'il y ait un problème au niveau de notre API. Veuillez retenter ultérieurement.")
+            });
+
+
     });
 }
 postForm();
