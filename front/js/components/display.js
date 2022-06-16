eb5daab86be8a037eba083dae5dd3ce925f043ed
Clement Lecocq <hello@clementlecocq.com>
2022-06-16 17:12:21 +0200

    (COMMENT) légère modif commentaire

--- front/js/components/display.js
+++ front/js/components/display.js
@@ -28,7 +28,7 @@ class DisplayProducts {
         const parser = new DOMParser();
         const items = document.getElementById('items');
         for (let i = 0; i < products.length; i++) {
-            let productsItems = //On insère le HTML dans le DOM
+            let productsItems = //We insert the HTML in the DOM
                 `<a href="./product.html?id=${products[i]._id}"> 
                 <article>
                 <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
